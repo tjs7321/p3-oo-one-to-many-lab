df0043747e5d6558508d3883923aa80b1cbbd246
Theodore Smith <tjs7321@gmail.com>
2023-08-21 02:26:14 -0400

    done

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,33 @@
 class Pet:
-    pass
+
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        self.name = name
+        self.owner = owner
+        if pet_type in Pet.PET_TYPES:
+            self.pet_type = pet_type
+        else:
+            raise Exception("Sorry, not a valid pet type.")
+        Pet.all.append(self)
 
 class Owner:
-    pass
+
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+    
+    def add_pet(self, pet):
+        if isinstance(pet, Pet):
+            pet.owner = self
+        else:
+            raise TypeError('Pet must be an instance of Pet class.')
+
+    def get_sorted_pets(self):
+        def name_sort(e):
+            return e.name
+        return sorted([pet for pet in Pet.all if pet.owner == self], key=name_sort)
